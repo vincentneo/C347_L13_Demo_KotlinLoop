1ffa30eac0678f17e60f0fef4c8ee3e411c2b51a
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-17 11:50:41 +0800

    implement 1-5 printing using for loop

--- app/src/main/java/sg/edu/rp/c347/id19007966/demokotlinloop/MainActivity.kt
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demokotlinloop/MainActivity.kt
@@ -2,10 +2,18 @@ package sg.edu.rp.c347.id19007966.demokotlinloop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener {
+            for (i in 1..5) {
+                Log.d("MainActivity", i.toString())
+            }
+        }
     }
 }
