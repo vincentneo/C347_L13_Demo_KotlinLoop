92a16188d30a92c666172d5e47d824af63e0d485
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2021-08-17 11:43:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c347/id19007966/demokotlinloop/MainActivity.kt
@@ -0,0 +1,11 @@
+package sg.edu.rp.c347.id19007966.demokotlinloop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
